6b3cd96e73fce38fff8bc33f89ca3188fa27174e
cynthia chebet <chebetcynthia8@gmail.com>
2021-07-26 11:49:33 +0300

    created the service

--- Client/src/app/services/patients.service.ts
+++ Client/src/app/services/patients.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
 
-const baseURL = 'http://localhost:8000/patients';
+const baseURL = 'http://localhost:8000/api/';
 @Injectable({
   providedIn: 'root',
 })
 export class PatientsService {
-  constructor() {}
+  constructor(private httpClient :HttpClient) {}
+
+  fetchPatient(): Observable<any>{
+    return this.httpClient.get(`${baseURL}patients`);
+  }
+
+  
 }
