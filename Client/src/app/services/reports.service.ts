7104937b61c3d066eb2a1304b43501288f76ff26
cynthia chebet <chebetcynthia8@gmail.com>
2021-07-27 17:00:39 +0300

    created reports service

---
+++ Client/src/app/services/reports.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const baseURL = 'http://localhost:8000/api';
+@Injectable({
+  providedIn: 'root',
+})
+export class ReportsService {
+  constructor(private httpClient: HttpClient) {}
+
+  //hiv monthly report
+  getHivMonthlyReport(month:number): Observable<any> {
+    return this.httpClient.get(`${baseURL}/hivReport/${month}`);
+  }
+
+  //hiv positive patients list
+  getHivPositivePatients(location:number,month:number): Observable<any>{
+    return this.httpClient.get(`${baseURL}/positivepatientlist/${location}/${month}`)
+  }
+
+  //hiv negative patients list
+  getHivNegativePatients(location:number,month:number):Observable<any>{
+    return this.httpClient.get(`${baseURL}/negativepatientlist/${location}/${month}`)
+  }
+}
